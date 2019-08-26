ec12cd3fcbb05c701380a142a2cfea9a3c607554
Patrick Roche <rochep63@gmail.com>
2019-08-26 14:28:48 -0400

    more work on core.Library class

--- commands/library.py
+++ commands/library.py
@@ -4,13 +4,17 @@ import core
 
 class Library(Command):
 
-    def create(self):
-        pass
+    def create(self, fn):
+        lib = core.Library()
+        lib.opendir(fn)
 
     def list(self):
         pass
 
     def call(self, flags=[], opts=[]):
-        print("library")
+        if len(opts) < 2:
+            return
+        if opts[0] == "create":
+            self.create(opts[1])
 
 Registry.insert("library", Library())
--- core/library.py
+++ core/library.py
@@ -1,5 +1,42 @@
 import pickle
+import tarfile
+import os
 
 class Library:
     def __init__(self):
+        self.name = ""
+        self.version = ""
+        self.author = ""
+        self.maintainer = ""
+        self.archive = ""
         self.files = []
+
+    def parse_propertes(self, props):
+        for line in props:
+            if line.startswith("name="):
+                self.name = line.split("=")[1][:-1]
+            elif line.startswith("version="):
+                self.version = line.split("=")[1][:-1]
+            elif line.startswith("author="):
+                self.author = line.split("=")[1][:-1]
+            elif line.startswith("maintainer="):
+                self.maintainer = line.split("="):[1][:-1]
+
+
+    def opendir(self, fn):
+        # check for valid library properties
+        props = []
+        with open(os.path.join(fn, "library.properties"), "r") as fp:
+            props.extend(fp.readlines())
+
+        # parse properties
+        self.parse_propertes(props)
+
+        # list all library files
+        for (dirpath, dirnames, filenames) in os.walk(fn):
+            for name in filenames:
+                self.files.append(os.path.join(dirpath, name))
+
+        # create archive
+
+        # pickle and write
